6a580a44bda09124dd92cb02f13dfabcb928fb2f
jmoreno844 <jmoreno844.dev@gmail.com>
2025-04-08 17:42:51 -0500

    Add debug to test.py

--- fastapi_project/app/config/test.py
+++ fastapi_project/app/config/test.py
@@ -2,8 +2,7 @@ from app.config.base import Settings
 
 
 class DeploySettings(Settings):
-    DEBUG: bool = False
-    TITLE: str = "FastAPI Project - Cloud"
+    DEBUG: bool = True
     POSTGRES_DB: str = "cloud_db"
     APP_ENV: str = "cloud"
     # Update with your actual deployed domain
